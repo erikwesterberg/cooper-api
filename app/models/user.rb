794c95cc487d67b31434c861ac2356d06bd62714
erka <erik.westerberg@hotmail.se>
2019-08-07 09:39:59 +0200

    installed devise gem, installation is tough not running proberly, going back and making a new branch

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  include DeviseTokenAuth::Concerns::User
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
+
